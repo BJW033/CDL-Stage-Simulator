887d9a7d4a1488b2d39810018c10bd9569a63069
BJW033 <Birdfan002@gmail.com>
2021-06-29 17:15:10 -0400

    Added JavaDoc to team class, moved getters and setters to bottom of class

--- src/model/team.java
+++ src/model/team.java
@@ -1,25 +1,38 @@
 package model;
 import java.util.ArrayList;
-
+/**
+ * The team class contains all the information for each team including: name, match wins and loses, map wins and loses, list of Matches,
+ * whether or not the team is in a tie (and how many ways the team is tied), and two lists of placings following simulations 
+ * @author bwu
+ *
+ */
 public class team implements Comparable<team>{
 	
-		String name;
-		int matchW;
-		int matchL;
-		int mapW;
-		int mapL;
-		ArrayList<Match> matches =new ArrayList<Match>();
-		double matchWinper;
-		boolean deadtie=false;
-		 boolean deadtie2=false;
-		boolean deadtie3=false;
-		boolean deadtie4=false;
-		boolean deadtie5=false;
-		boolean deadtie6=false;
-		ArrayList<Double> placings = new ArrayList<Double>(); 
-		ArrayList<Double> placings2 = new ArrayList<Double>(); 
-		double top3Per=0;
-
+		private String name;
+		private int matchW;
+		private int matchL;
+		private int mapW;
+		private int mapL;
+		private ArrayList<Match> matches =new ArrayList<Match>();
+		private double matchWinper;
+		private boolean deadtie2=false;
+		private boolean deadtie3=false;
+		private boolean deadtie4=false;
+		private boolean deadtie5=false;
+		private boolean deadtie6=false;
+		private ArrayList<Double> placings = new ArrayList<Double>(); 
+		private ArrayList<Double> placingsSim = new ArrayList<Double>();
+/**
+ * Constructor for team needing a string for the team name and five strings of the other five teams of the pool. 
+ * Creates matches for the team and each of their opponents, and sets their placings and placingsSim to to ArrayList of
+ * size six filled with zeros (doubles). 
+ * @param team String the team name of the object. 
+ * @param op1 String name of opponent one 
+ * @param op2 String name of opponent two 
+ * @param op3 String name of opponent three 
+ * @param op4 String name of opponent four 
+ * @param op5 String name of opponent five 
+ */
 		public team(String team,String op1,String op2,String op3,String op4,String op5) {
 			name = team;
 			matchW=0;
@@ -37,9 +50,13 @@ public class team implements Comparable<team>{
 				placings.add((double) 0);
 			}
 			for(int i = 0;i<6;i++) {
-				placings2.add((double) 0);
+				placingsSim.add((double) 0);
 			}
 		}
+		/**
+		 * Prints the number of times from a simulation that a team landed in each seed of the pool.
+		 * @param longName int the length of the longest name in the pool, used for formatting the print.
+		 */
 		public void printPlacings(int longName) {
 
 			System.out.print(getName() + " simulated placings count: ");
@@ -55,9 +72,12 @@ public class team implements Comparable<team>{
 
 
 		}
-		public ArrayList<Double> getPlacings() {
-			return placings;
-		}
+		
+		/**
+		 * Prints the percent of simulations that a team landed in each seed of the pool.
+		 * @param sims int number of simulation run
+		 * @param longName int the length of the longest name in the pool, used for formatting the print. 
+		 */
 		void printSimPer(int sims, int longName) {
 
 			System.out.print(getName() + " simulated placings percent: ");
@@ -77,12 +97,15 @@ public class team implements Comparable<team>{
 
 		}
 
-		public void setTie(boolean b) {
-			deadtie=true;
-		}
+		/**
+		 * Dynamic setter, that takes in a boolean value and an interger to dynamically set the number of teams the team is in a dead tie with
+		 * not very frequently does this happen.
+		 * @param b boolean value to be set
+		 * @param i int number of teams the team is tied with
+		 */
 		public void setTie(boolean b,int i) {
 			if(i==2) {
-				setDeadtie2(b);
+				deadtie2=b;
 			}
 			else if(i==3) {
 				deadtie3=b;
@@ -98,6 +121,10 @@ public class team implements Comparable<team>{
 			}
 		}
 
+		/**
+		 * Calculates and returns a team's map win percentage
+		 * @return double map win percentage
+		 */
 		public double getmapWinper() {
 			if(mapW+mapL == 0) {
 				return 0;
@@ -105,17 +132,22 @@ public class team implements Comparable<team>{
 			return (double)mapW/(mapW+mapL);
 		}
 
-		public ArrayList<Match> getMatches() {
-
-			return matches;
-		}
-
-		void printMatches() {
+		
+/**
+ * Prints all of the teams matches and results
+ */
+		public void printMatches() {
 			System.out.println(getName()+"'s matches: ");
 			matches.forEach((m) -> System.out.println(m));
 		}
-
-		void updateMatches(String opp, int teamAmaps, int teamBmaps) {
+/**
+ * Iterates through the team's matches and finds the match verse opp. Updates the map count in the Match object. Updated the team's 
+ * overall map count, match count, and match win percentage. 
+ * @param opp String opponent name
+ * @param teamAmaps int number of maps current team won
+ * @param teamBmaps int number of maps the opponent won
+ */
+		public void updateMatches(String opp, int teamAmaps, int teamBmaps) {
 			for(Match i: matches) {
 				if(i.B.equals(opp)) {
 					i.updateMatch(teamAmaps, teamBmaps);
@@ -135,12 +167,21 @@ public class team implements Comparable<team>{
 
 		}
 
-		void printMapCount() {
+		/**
+		 * Prints team map count.
+		 */
+		public void printMapCount() {
 			System.out.println(getName() +" has a " + mapW + "-" + mapL +" map count.");
 		}
+		/**
+		 * Prints team match count
+		 */
 		void printRecord() {
 			System.out.println(getName() +" has a " + matchW + "-" + matchL +" match count.");
 		}
+		/**
+		 * Prints the team full record with match and map count with tie indicators
+		 */
 		void printRecordShort() {
 			System.out.print(getName() +": (Matches) " + matchW + "-" + matchL + " (Maps) "+ mapW + "-" + mapL);
 			if(isDeadtie2()||deadtie3||deadtie4||deadtie5||deadtie6) {
@@ -164,7 +205,9 @@ public class team implements Comparable<team>{
 			System.out.println();
 
 		}
-
+/**
+ * Uses the team's match win percentage to provide a basic sort of team in descending order
+ */
 		@Override
 		public int compareTo(team comparestu) {
 			double compareMatchWinper=((team)comparestu).getmatchWinper();
@@ -180,36 +223,26 @@ public class team implements Comparable<team>{
 			return (int) ((compareMatchWinper-this.matchWinper)*10);
 
 		}
-
-		public int getmatchW() {
-			return matchW;
-		}
-
-		public int getmatchL() {
-			return matchL;
-		}
-
-		public double getmatchWinper() {
-			return matchWinper;
-		}
-		public String getName() {
-			return name;
-		}
-		public ArrayList<Double> getPlacings2() {
-			return placings2;
-		}
-		public void setPlacings2(ArrayList<Double> placings2) {
-			this.placings2 = placings2;
-		}
-		
-		public Match findMatch(String s) {
+	
+	/**
+	 * Searches the list of matches for a provided opponent 
+	 * @param op String name of of opponent of match
+	 * @return Match if a Match verse op exists, null otherwise. 
+	 */
+		public Match findMatch(String op) {
 			for(Match m: matches) {
-				if(s.equals(m.B)) {
+				if(op.equals(m.B)) {
 					return m;
 				}
 			}
 			return null;
 		}
+		
+		/**
+		 * Resets the match verse particular opponent. Removes the match maps from the team's map count and removes the 
+		 * team's match from its match count.
+		 * @param opp
+		 */
 		public void removeMatch(String opp) {
 			for(Match m: matches) {
 				if(m.getB().equals(opp)) {
@@ -229,28 +262,83 @@ public class team implements Comparable<team>{
 			}
 		}
 		
+		/**
+		 * Prints if the team is in any kind of tie.
+		 */
 		public void printTies() {
-			if(isDeadtie2()||deadtie3||deadtie4||deadtie5||deadtie6) {
+			if(deadtie2||deadtie3||deadtie4||deadtie5||deadtie6) {
 				System.out.print("Ties present");
 			}
 		}
 		
-		public double getTop3PerSim() {
-			return placings2.get(0)+placings2.get(1)+placings2.get(2);
-		}
+		
+		/**
+		 * Returns the number of times the team placed in the top three in the first Simulation ArrayList
+		 * @return double sum of first three elements of placings
+		 */
 		public double getTop3Per() {
 			return placings.get(0)+placings.get(1)+placings.get(2);
+		}
+		/**
+		 * Returns the number of times the team placed in the top three in the second Simulation ArrayList
+		 * @return double sum of first three elements of placingsSim
+		 */
+		public double getTop3PerSim() {
+			return placingsSim.get(0)+placingsSim.get(1)+placingsSim.get(2);
+		}
+		
+		
+		public ArrayList<Double> getPlacings() {
+			return placings;
 		}
 		public boolean isDeadtie2() {
 			return deadtie2;
 		}
-		public void setDeadtie2(boolean deadtie2) {
-			this.deadtie2 = deadtie2;
-		}
+		
 		public boolean isDeadtie3() {
 			return deadtie3;
 		}
-		
+		public boolean isDeadtie4() {
+			return deadtie4;
+		}
+		public boolean isDeadtie5() {
+			return deadtie5;
+		}
+		public boolean isDeadtie6() {
+			return deadtie6;
+		}
+		public ArrayList<Double> getPlacingsSim() {
+			return placingsSim;
+		}
+		public void setMatchW(int matchW) {
+			this.matchW = matchW;
+		}
+		public void setMatchL(int matchL) {
+			this.matchL = matchL;
+		}
+		public void setMapW(int mapW) {
+			this.mapW = mapW;
+		}
+		public void setMapL(int mapL) {
+			this.mapL = mapL;
+		}
+		public ArrayList<Match> getMatches() {
+			return matches;
+		}
+		public int getmatchW() {
+			return matchW;
+		}
+
+		public int getmatchL() {
+			return matchL;
+		}
+
+		public double getmatchWinper() {
+			return matchWinper;
+		}
+		public String getName() {
+			return name;
+		}
 
 	}
 
